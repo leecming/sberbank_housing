f58279cddf180cfb2695c905c401ab7ede29ceb6
leecming <leecming@gmail.com>
2019-12-05 07:28:53 +0800

    Added macro indicator de-meaning and moved common functions from individual models to here (split folds & generate targets for distributional models)

--- preprocessors.py
+++ preprocessors.py
@@ -1,11 +1,19 @@
 """ Pre-processing helper functions """
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
+from scipy.stats import truncnorm
 
 TRAIN_PATH = 'data/train.csv'
 TEST_PATH = 'data/test.csv'
 MACRO_PATH = 'data/macro.csv'
+
+
+def split_to_folds(input_df, num_folds, seed=None):
+    """ Split input df into kfolds returning (train, val) index tuples """
+    kf = KFold(n_splits=num_folds, random_state=seed, shuffle=True)
+    return [x for x in kf.split(input_df)]
 
 
 def window_stack(a, step_size=1, width=3):
@@ -44,11 +52,79 @@ def generate_macro_windows(min_unique=100, lookback_period=100):
     return rolling_dates, rolling_matrix
 
 
+def generate_target_dist(mean, num_bins, low, high):
+    """
+    Generate discretized truncated norm prob distribution centered around mean
+    :param mean: center of truncated norm
+    :param num_bins: number of bins
+    :param low: low end of truncated range
+    :param high: top end of truncated range
+    :return: (support, probabilities for support) tuple
+    """
+    radius = 0.5 * (high - low) / num_bins
+
+    def trunc_norm_prob(center):
+        """ get probability mass """
+        return (truncnorm.cdf(center + radius,
+                              a=(low - mean) / radius,
+                              b=(high - mean) / radius,
+                              loc=mean, scale=radius) -
+                truncnorm.cdf(center - radius,
+                              a=(low - mean) / radius,
+                              b=(high - mean) / radius,
+                              loc=mean, scale=radius))
+
+    supports = np.array([x * (2 * radius) + radius + low for x in range(num_bins)])
+    probs = np.array([trunc_norm_prob(support) for support in supports])
+    return supports, probs
+
+
+def demean_by_macro_indicator(train_df, indicator='cpi'):
+    """
+    divide price_doc by CPI for that given date
+    """
+    macro_df = pd.read_csv(MACRO_PATH).iloc[:-1].fillna(method='ffill').fillna(method='bfill')
+    macro_df = macro_df.fillna(method='ffill').fillna(method='bfill')
+    # norm against their values on the 1st date of the macro time series
+    numeric_cols = [col for col in macro_df.columns if macro_df[col].dtype in [np.int, np.float]]
+    macro_df[numeric_cols] /= macro_df[numeric_cols].iloc[0]
+
+    macro_df['timestamp'] = pd.to_datetime(macro_df['timestamp'])
+    macro_df = macro_df.set_index('timestamp')[indicator]
+    train_df = train_df.set_index('timestamp').join(macro_df)
+    train_df['price_doc'] = train_df['price_doc'] / train_df[indicator]
+    train_df = train_df.drop(indicator, axis=1).reset_index()
+    train_df['timestamp'] = train_df['timestamp'].dt.strftime('%Y-%m-%d')
+    return train_df
+
+
+def remean_price_by_indicator(output_df, indicator):
+    """ for a model trained by a demeaned target, need to remean the predictions"""
+    timestamp_df = pd.read_csv(TRAIN_PATH).set_index('id')['timestamp']
+    timestamp_df = timestamp_df.append(pd.read_csv(TEST_PATH).set_index('id')['timestamp'])
+    output_df = output_df.set_index('id').join(timestamp_df).reset_index()
+    output_df['timestamp'] = pd.to_datetime(output_df['timestamp'])
+    output_df = output_df.set_index('timestamp')
+
+    indicator_df = pd.read_csv('data/macro.csv').iloc[:-1].fillna(method='ffill').fillna(method='bfill')
+    # norm against their values on the 1st date of the macro time series
+    numeric_cols = [col for col in indicator_df.columns if indicator_df[col].dtype in [np.int, np.float]]
+    indicator_df[numeric_cols] /= indicator_df[numeric_cols].iloc[0]
+    indicator_df['timestamp'] = pd.to_datetime(indicator_df['timestamp'])
+    indicator_df = indicator_df.set_index('timestamp')[indicator]
+
+    output_df = output_df.join(indicator_df)
+    output_df['price_doc'] = output_df['price_doc'] * output_df[indicator]
+    return output_df.reset_index(drop=True).drop(indicator, axis=1)
+
+
 def preprocess_csv(ohe_features=False,
                    ohe_card=10,
                    generate_rolling=False,
                    min_unique=100,
-                   lookback_period=100):
+                   lookback_period=100,
+                   demean_by_indicator=False,
+                   demeaning_indicator='cpi'):
     """
     Transforms raw data in input CSVs into features ready for modelling
     1. Drop id column
@@ -62,6 +138,10 @@ def preprocess_csv(ohe_features=False,
     # infer arg needed otherwise int columns get casted as float
     train_df = train_df.fillna(-1, downcast='infer')
     train_df['is_train'] = 1
+
+    # demean by CPI
+    if demean_by_indicator:
+        train_df = demean_by_macro_indicator(train_df, indicator=demeaning_indicator)
 
     test_df = test_df.fillna(-1, downcast='infer')
     test_df['is_train'] = 0
